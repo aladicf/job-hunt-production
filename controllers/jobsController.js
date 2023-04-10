4578ca8ff729dd3bbc4076f2b2d5f174a8848d0f
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-10 19:42:30 +0200

    Update jobsController.js

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -113,71 +113,47 @@ const showStats = async (req, res) => {
     const { user: { userId } } = req
 
     let stats = await Job.aggregate([
-        { $match: { createdBy: mongoose.Types.ObjectId(userId) } },
-        { $group: { _id: '$status', count: { $sum: 1 } } },
-        {
-            $addFields: {
-                pendingCount: { $cond: [{ $eq: ['$_id', 'pending'] }, '$count', 0] },
-                interviewCount: { $cond: [{ $eq: ['$_id', 'interview'] }, '$count', 0] },
-                declinedCount: { $cond: [{ $eq: ['$_id', 'declined'] }, '$count', 0] },
-            },
-        },
-        {
-            $group: {
-                _id: null,
-                pendingCountTotal: { $sum: '$pendingCount' },
-                interviewCountTotal: { $sum: '$interviewCount' },
-                declinedCountTotal: { $sum: '$declinedCount' },
-            },
-        },
-    ])
+		{ $match: { createdBy: mongoose.Types.ObjectId(userId) } },
+		{ $group: { _id: '$status', count: { $sum: 1 } } },
+	])
+	stats = stats.reduce((acc, curr) => {
+		const { _id: title, count } = curr
+		acc[title] = count
+		return acc
+	}, {})
 
 	const defaultStats = {
-        pending: stats[0].pendingCountTotal,
-        interview: stats[0].interviewCountTotal,
-        declined: stats[0].declinedCountTotal,
-    }
+		pending: stats.pending || 0,
+		interview: stats.interview || 0,
+		declined: stats.declined || 0,
+	}
 
 	let monthlyApplications = await Job.aggregate([
-        { $match: { createdBy: mongoose.Types.ObjectId(userId) } },
-        {
-            $group: {
-                _id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt' } },
-                count: { $sum: 1 },
-            },
-        },
-        { $sort: { '_id.year': -1, '_id.month': -1 } },
-        { $limit: 12 },
-        {
-            $project: {
-                _id: 0,
-                date: {
-                    $dateToString: {
-                        format: '%B %Y',
-                        date: {
-                            $dateFromParts:
-                                {
-                                    year:
-                                        '$_id.year',
-                                    month:
-                                        '$_id.month',
-                                }
-                            ,
-                        }
-                        ,
-                    }
-                    ,
-                }
-                ,
-                count:
-                    1,
-            }
-            ,
-        }
-        ,
-    ])
+		{ $match: { createdBy: mongoose.Types.ObjectId(userId) } },
+		{
+			$group: {
+				_id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt' } },
+				count: { $sum: 1 },
+			},
+		},
+		{ $sort: { '_id.year': -1, '_id.month': -1 } },
+		{ $limit: 12 },
+	])
+	monthlyApplications = monthlyApplications
+		.map((item) => {
+			const {
+				_id: { year, month },
+				count,
+			} = item
+			const date = moment()
+				.month(month - 1)
+				.year(year)
+				.format('MMMM Y')
+			return { date, count }
+		})
+		.reverse()
 
-    res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications })
+	res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications })
 }
 
 export { createJob, deleteJob, getAllJobs, updateJob, showStats }
