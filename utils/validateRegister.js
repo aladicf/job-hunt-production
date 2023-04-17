0320601d0d63d6609e10725bc51e4a1d92f4cc63
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 16:28:25 +0200

    Update validateRegister.js

--- utils/validateRegister.js
+++ utils/validateRegister.js
@@ -1,17 +1,24 @@
+
 import { BadRequestError } from '../errors/index.js'
 
+// Defining a function to validate the registration fields
 const validateRegisterFields = (name, email, password,) => {
+    // Checking if all values are provided
     if (!name || !email || !password) {
+        // Throwing an error if any value is missing
         throw new BadRequestError('Please provide all values')
     }
 
-    // Validate email format
+    // Validate email format using a regular expression
     const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (!emailRegex.test(email)) {
+        // Throwing an error if the email format is invalid
         throw new BadRequestError('Invalid email format')
     }
 
+    // Checking if the password is at least 8 characters long
     if (password.length < 8) {
+        // Throwing an error if the password is too short
         throw new BadRequestError('Password must be at least 8 characters')
     }
 }
