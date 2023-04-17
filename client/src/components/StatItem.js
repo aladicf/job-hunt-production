600a188a1a6395e6278f36876a44007ddbbcc562
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 21:49:24 +0200

    Update StatItem.js

--- client/src/components/StatItem.js
+++ client/src/components/StatItem.js
@@ -2,16 +2,16 @@ import Wrapper from '../assets/wrappers/StatItem'
 
 // Define a functional component called StatsItem that takes props count, title, icon, color and bcg
 const StatsItem = ({ count, title, icon, color, bcg }) => {
-	return (
-		{/* Return a Wrapper component and pass color and bcg props */ }
-		<Wrapper color={color} bcg={bcg}>
-			<header>
-				<span className='count'>{count}</span>
-				<span className='icon'>{icon}</span>
-			</header>
-			<h5 className='title'>{title}</h5>
-		</Wrapper>
-	)
+  // Return a Wrapper component and pass color and bcg props
+  return (
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className='count'>{count}</span>
+        <span className='icon'>{icon}</span>
+      </header>
+      <h5 className='title'>{title}</h5>
+    </Wrapper>
+  )
 }
 
 export default StatsItem
