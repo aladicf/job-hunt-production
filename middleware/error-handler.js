4167f1042ba81c19b1a1357e0fdf165d5aab072b
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 16:41:39 +0200

    Update error-handler.js

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,23 +1,35 @@
 import { StatusCodes } from 'http-status-codes'
 
+// Defining an error handling middleware function
 const errorHandlerMiddleware = (err, req, res, next) => {
-	const defaultError = {
-		statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-		msg: err.message || 'Something went wrong, try again later',
-	}
-	if (err.name === 'ValidationError') {
-		defaultError.statusCode = StatusCodes.BAD_REQUEST
+    // Creating a default error object
+    const defaultError = {
+        // Setting the default status code and message
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || 'Something went wrong, try again later',
+    }
 
-		defaultError.msg = Object.values(err.errors)
-			.map((item) => item.message)
-			.join(',')
-	}
-	if (err.code && err.code === 11000) {
-		defaultError.statusCode = StatusCodes.BAD_REQUEST
-		defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`
-	}
+    // Checking if the error is a validation error
+    if (err.name === 'ValidationError') {
+        // Setting the status code to Bad Request
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
 
-	res.status(defaultError.statusCode).json({ msg: defaultError.msg })
+        // Setting the message to the validation error messages
+        defaultError.msg = Object.values(err.errors)
+            .map((item) => item.message)
+            .join(',')
+    }
+
+    // Checking if the error is a duplicate key error
+    if (err.code && err.code === 11000) {
+        // Setting the status code to Bad Request
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        // Setting the message to indicate that a field has to be unique
+        defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`
+    }
+
+    // Sending the error response
+    res.status(defaultError.statusCode).json({ msg: defaultError.msg })
 }
 
 export default errorHandlerMiddleware
