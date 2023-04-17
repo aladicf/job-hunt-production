aa04033367a72ea0fcf7271927308dfa1f4b1f8a
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 16:23:57 +0200

    Update validateUpdate.js

--- utils/validateUpdate.js
+++ utils/validateUpdate.js
@@ -1,17 +1,20 @@
+
 import { BadRequestError } from '../errors/index.js'
 
+// Defining a function to validate the update fields
 const validateUpdateFields = (name, email, lastName, location) => {
+    // Checking if all values are provided
     if (!name || !email || !lastName || !location) {
+        // Throwing an error if any value is missing
         throw new BadRequestError('Please provide all values')
     }
 
-    // Validate email format
+    // Validate email format using a regular expression
     const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (!emailRegex.test(email)) {
+        // Throwing an error if the email format is invalid
         throw new BadRequestError('Invalid email format')
     }
-
-    
 }
 
-export default validateUpdateFields
+export default validateUpdateFieldselds
