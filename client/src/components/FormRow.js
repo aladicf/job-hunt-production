3cde6e0b69e56916dd140b80f8e03f3f5fa3ad1f
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 19:02:27 +0200

    Update FormRow.js

--- client/src/components/FormRow.js
+++ client/src/components/FormRow.js
@@ -1,9 +1,14 @@
+
+// Define a functional component named FormRow that takes in several props
 const FormRow = ({ type, name, value, handleChange, labelText }) => {
+	// Return a div element with a className of form-row
 	return (
 		<div className='form-row'>
+			{/* Render a label element with specified htmlFor and className props and display either the value of labelText or name */}
 			<label htmlFor={name} className='form-label'>
 				{labelText || name}
 			</label>
+			{/* Render an input element with specified type, value, name, onChange and className props */}
 			<input
 				type={type}
 				value={value}
