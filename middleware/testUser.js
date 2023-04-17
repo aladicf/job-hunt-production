84b1b59105459aff6fa4ae67c29028080e90a168
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 16:40:05 +0200

    Update testUser.js

--- middleware/testUser.js
+++ middleware/testUser.js
@@ -1,10 +1,14 @@
 import { BadRequestError } from '../errors/index.js'
 
+// Defining a middleware function to check if the user is a test user
 const testUser = (req, res, next) => {
-	if (req.user.testUser) {
-		throw new BadRequestError('Test user is read only mode for this page !')
-	}
-	next()
+    // Checking if the user is a test user
+    if (req.user.testUser) {
+        // Throwing an error if the user is a test user
+        throw new BadRequestError('Test user is read only mode for this page !')
+    }
+    // Calling the next middleware function in the stack
+    next()
 }
 
 export default testUser
