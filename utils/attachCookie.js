0c84c91287089b09d743c7e31b6ca75bf40bb9aa
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 16:32:06 +0200

    Update attachCookie.js

--- utils/attachCookie.js
+++ utils/attachCookie.js
@@ -1,11 +1,16 @@
+// Defining a function to attach a cookie to the response object
 const attachCookie = ({ res, token }) => {
-	const oneMonth = 1000 * 60 * 60 * 24 * 30
+    // Calculating the duration of one month in milliseconds
+    const oneMonth = 1000 * 60 * 60 * 24 * 30
 
-	res.cookie('token', token, {
-		httpOnly: true,
-		expires: new Date(Date.now() + oneMonth),
-		secure: process.env.NODE_ENV === 'production',
-	})
+    // Attaching the token as a cookie to the response object
+    res.cookie('token', token, {
+        httpOnly: true,
+        // Setting the expiration date of the cookie to one month from now
+        expires: new Date(Date.now() + oneMonth),
+        // Setting the secure flag to true if the environment is production
+        secure: process.env.NODE_ENV === 'production',
+    })
 }
 
 export default attachCookie
