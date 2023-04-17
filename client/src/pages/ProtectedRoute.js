1e6e3551cfbdd4354dae1b279576a72a88f26f78
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 21:10:20 +0200

    Update ProtectedRoute.js

--- client/src/pages/ProtectedRoute.js
+++ client/src/pages/ProtectedRoute.js
@@ -2,12 +2,12 @@ import { useAppContext } from '../context/appContext'
 import { Navigate } from 'react-router-dom'
 import Loading from '../components/Loading'
 
-{/* Define ProtectedRoute component */ }
+// Define ProtectedRoute component
 const ProtectedRoute = ({ children }) => {
-	{/* Get user and userLoading values from app context */ }
+	// Get user and userLoading values from app context
 	const { user, userLoading } = useAppContext()
 
-	{/* If userLoading is true, render Loading component */ }
+	// If userLoading is true, render Loading component
 	if (userLoading)
 		return (
 			<div>
