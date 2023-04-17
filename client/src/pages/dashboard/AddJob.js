235f2abff036cfae432ade513640e01f0a63aa98
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 18:14:04 +0200

    Update AddJob.js

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -21,27 +21,39 @@ const AddJob = () => {
 		createJob,
 	} = useAppContext()
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
+	// Define handleSubmit function
+const handleSubmit = (e) => {
+	// Prevent default form submission behavior
+	e.preventDefault()
 
-		if (!position || !company || !jobLocation) {
-			displayAlert()
-			return
-		}
-		if (isEditing) {
-			editJob()
-		}
-		createJob()
+	// Check if position, company, and jobLocation values are defined
+	if (!position || !company || !jobLocation) {
+		// Call displayAlert function
+		displayAlert()
+		return
 	}
-		const handleJobInput = ({ target: { name, value } }) => {
-  			handleChange({ name, value });
-		}
+	// If isEditing is true, call editJob function
+	if (isEditing) {
+		editJob()
+	}
+	// Call createJob function
+	createJob()
+}
+
+// Define handleJobInput function
+const handleJobInput = ({ target: { name, value } }) => {
+	// Call handleChange function with name and value properties
+	handleChange({ name, value });
+}
 
 	return (
 		<Wrapper>
 			<form className='form'>
+				{/* Heading displaying either "edit job" or "add job" */}
 				<h3>{isEditing ? 'edit job' : 'add job'}</h3>
+				{/* Conditional rendering of Alert component */}
 				{showAlert && <Alert />}
+				{/* FormRow components for position, company, and jobLocation inputs */}
 				<div className='form-center'>
 					<FormRow
 						type='text'
@@ -64,7 +76,8 @@ const AddJob = () => {
 						value={jobLocation}
 						handleChange={handleJobInput}
 					/>
-
+					
+					{/* FormRowSelect components for status and jobType inputs */}
 					<FormRowSelect
 						name='status'
 						value={status}
@@ -79,7 +92,8 @@ const AddJob = () => {
 						handleChange={handleJobInput}
 						list={jobTypeOptions}
 					/>
-
+					
+					{/* Container with submit and clear buttons */}
 					<div className='btn-container'>
 						<button
 							type='submit'
