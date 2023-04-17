f0c93e72622ba2d4a1dfee0619ac96006bb74bde
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 19:14:56 +0200

    Update PageBtnContainer.js

--- client/src/components/PageBtnContainer.js
+++ client/src/components/PageBtnContainer.js
@@ -2,12 +2,16 @@ import { useAppContext } from '../context/appContext'
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 import Wrapper from '../assets/wrappers/PageBtnContainer'
 
+// Define a functional component named PageBtnContainer
 const PageBtnContainer = () => {
+	// Destructure numOfPages, page and changePage from the value returned by useAppContext hook
 	const { numOfPages, page, changePage } = useAppContext()
 
+	// Declare a variable named pages and initialize it to an array of numbers from 1 to numOfPages
 	const pages = Array.from({ length: numOfPages }, (_, index) => {
 		return index + 1
 	})
+	// Declare a function named nextPage that increments the page number and calls changePage with the new page number
 	const nextPage = () => {
 		let newPage = page + 1
 		if (newPage > numOfPages) {
@@ -15,6 +19,7 @@ const PageBtnContainer = () => {
 		}
 		changePage(newPage)
 	}
+	// Declare a function named prevPage that decrements the page number and calls changePage with the new page number
 	const prevPage = () => {
 		let newPage = page - 1
 		if (newPage < 1) {
@@ -22,13 +27,18 @@ const PageBtnContainer = () => {
 		}
 		changePage(newPage)
 	}
+	// Return a Wrapper component
 	return (
 		<Wrapper>
+			{/* Render a button element with specified className and onClick props */}
 			<button className='prev-btn' onClick={prevPage}>
+				{/* Render a FaArrowLeft component */}
 				<FaArrowLeft />
 				prev
 			</button>
+			{/* Render a div element with a className of btn-container */}
 			<div className='btn-container'>
+				{/* Map over the pages array and render a button element for each page number */}
 				{pages.map((pageNumber) => {
 					return (
 						<button
@@ -42,12 +52,12 @@ const PageBtnContainer = () => {
 					)
 				})}
 			</div>
+			{/* Render a button element with specified className and onClick props */}
 			<button className='next-btn' onClick={nextPage}>
 				next
+				{/* Render a FaArrowRight component */}
 				<FaArrowRight />
 			</button>
 		</Wrapper>
-	)
-}
 
 export default PageBtnContainer
