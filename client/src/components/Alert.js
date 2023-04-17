45c4b3df0027c99e4b026d0af8b61bcc00969a4b
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 18:52:02 +0200

    Update Alert.js

--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -1,7 +1,10 @@
 import { useAppContext } from '../context/appContext'
 
+// Define a functional component named Alert
 const Alert = () => {
+	// Destructure alertType and alertText from the value returned by useAppContext hook
 	const { alertType, alertText } = useAppContext()
+	// Return a div element with a className based on the alertType and display the alertText
 	return <div className={`alert alert-${alertType}`}>{alertText}</div>
 }
 
