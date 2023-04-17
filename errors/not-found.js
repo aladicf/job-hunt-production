f7d452072149669ebb9261b9dc71d8d340ee6aac
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 16:53:45 +0200

    Update not-found.js

--- errors/not-found.js
+++ errors/not-found.js
@@ -1,11 +1,15 @@
 import { StatusCodes } from 'http-status-codes'
 import CustomAPIError from './custom-api.js'
 
+// Define a NotFoundError class that extends the CustomAPIError class
 class NotFoundError extends CustomAPIError {
-	constructor(message) {
-		super(message)
-		this.statusCode = StatusCodes.NOT_FOUND
-	}
+  // Define the class constructor
+  constructor(message) {
+    // Call the super constructor with the message parameter
+    super(message)
+    // Set the statusCode property of the instance to StatusCodes.NOT_FOUND (HTTP status code 404)
+    this.statusCode = StatusCodes.NOT_FOUND
+  }
 }
 
 export default NotFoundError
