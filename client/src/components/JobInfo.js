c2adc2a3cbfc366f680c1368cd1d7fe8ce5fe66a
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 19:04:19 +0200

    Update JobInfo.js

--- client/src/components/JobInfo.js
+++ client/src/components/JobInfo.js
@@ -1,9 +1,13 @@
 import Wrapper from '../assets/wrappers/JobInfo'
 
+// Define a functional component named JobInfo that takes in two props
 const JobInfo = ({ icon, text }) => {
+	// Return a Wrapper component
 	return (
 		<Wrapper>
+			{/* Render a span element with a className of icon and display the value of icon */}
 			<span className='icon'>{icon}</span>
+			{/* Render a span element with a className of text and display the value of text */}
 			<span className='text'>{text}</span>
 		</Wrapper>
 	)
