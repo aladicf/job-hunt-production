38ead19267cc2f871b43d928978af1c184a5a8bd
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 19:07:59 +0200

    Update Logo.js

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,6 +1,8 @@
 import logo from '../assets/images/logo.svg'
 
+// Define a functional component named Logo
 const Logo = () => {
+	// Return an img element with specified src, alt, className, weight and height props
 	return (
 		<img src={logo} alt='job hunt' className='logo' weight={200} height={200} />
 	)
