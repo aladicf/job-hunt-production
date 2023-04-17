9fd4763ddcd9c9d55a71fab342045972d61b9132
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 16:30:40 +0200

    Create checkPermissions.js

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -1,9 +1,12 @@
 import { UnAuthenticatedError } from '../errors/index.js'
 
+// Defining a function to check if the user has permission to access a resource
 const checkPermissions = (requestUser, resourceUserId) => {
-	if (requestUser.userId === resourceUserId.toString()) return
+    // Checking if the user making the request is the same as the user who owns the resource
+    if (requestUser.userId === resourceUserId.toString()) return
 
-	throw new UnAuthenticatedError('Not authorized to access this route')
+    // Throwing an error if the user is not authorized to access the resource
+    throw new UnAuthenticatedError('Not authorized to access this route')
 }
 
 export default checkPermissions
