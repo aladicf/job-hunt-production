1ff7343d46d68db689704e1cf77e75487d206cb4
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 16:42:33 +0200

    Update not-found.js

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,6 +1,8 @@
 import { StatusCodes } from 'http-status-codes'
 
+// Defining a middleware function to handle requests to non-existent routes
 const notFoundMiddleware = (req, res) =>
-	res.status(StatusCodes.NOT_FOUND).send('Page does not exist')
+    // Sending a Not Found status code and a message indicating that the page does not exist
+    res.status(StatusCodes.NOT_FOUND).send('Page does not exist')
 
 export default notFoundMiddleware
