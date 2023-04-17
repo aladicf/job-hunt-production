68c4646af85a2e15166371744de48b9c075ad77e
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 16:46:20 +0200

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose'
 
+// Define an asynchronous function to connect to a MongoDB database using Mongoose
 const connectDB = async (url) => {
-	return mongoose.connect(url, { dbName: 'jobhunt' })
+  // Connect to the MongoDB database at the specified URL and specify the database name in the connection options
+  return mongoose.connect(url, { dbName: 'jobhunt' })
 }
 
 export default connectDB
