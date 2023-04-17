59d05de79456c71e24923c19c3c648edc5f3cb17
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 21:28:41 +0200

    Update links.js

--- client/src/utils/links.js
+++ client/src/utils/links.js
@@ -2,28 +2,16 @@ import { FaChartBar } from 'react-icons/fa'
 import { FaRegListAlt } from 'react-icons/fa'
 import { FaRegPlusSquare } from 'react-icons/fa'
 import { FaUserCog } from 'react-icons/fa'
-
-{/* Define an array of objects named links */}
+ks */}
 
 const links = [
-	{/* Each object in the array represents a link */}
     
     {
-	{/* The id property is a unique identifier for the link */}
-        
         id: 1,
-	{/* The text property is the text that will be displayed for the link */}
-        
         text: 'stats',
-	{/* The path property is the URL path that the link will navigate to when clicked */ }
-        
         path: '/',
-	{/* The icon property is a React element that will be rendered as the link's icon */ }
-        
         icon: <FaChartBar />,
     },
-	{/* Additional link objects with similar properties */ }
-    
 	{
 		id: 2,
 		text: 'all jobs',
