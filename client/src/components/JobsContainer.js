48b116186ca51db9c9d51ea645f0793e29674962
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 19:06:12 +0200

    Update JobsContainer.js

--- client/src/components/JobsContainer.js
+++ client/src/components/JobsContainer.js
@@ -6,7 +6,9 @@ import Alert from './Alert'
 import Wrapper from '../assets/wrappers/JobsContainer'
 import PageBtnContainer from './PageBtnContainer'
 
+// Define a functional component named JobsContainer
 const JobsContainer = () => {
+	// Destructure several values from the value returned by useAppContext hook
 	const {
 		getJobs,
 		jobs,
@@ -20,14 +22,17 @@ const JobsContainer = () => {
 		numOfPages,
 		showAlert,
 	} = useAppContext()
+	// Use the useEffect hook to call getJobs when the component mounts or when any of the specified dependencies change
 	useEffect(() => {
 		getJobs()
 		// eslint-disable-next-line
 	}, [page, search, searchStatus, searchType, sort])
+	// If isLoading is true, return a Loading component with a center prop
 	if (isLoading) {
 		return <Loading center />
 	}
 
+	// If the length of jobs is 0, return a Wrapper component with a message
 	if (jobs.length === 0) {
 		return (
 			<Wrapper>
@@ -36,17 +41,23 @@ const JobsContainer = () => {
 		)
 	}
 
+	// Return a Wrapper component
 	return (
 		<Wrapper>
+			{/* Conditionally render an Alert component if showAlert is true */}
 			{showAlert && <Alert />}
+			{/* Render a h5 element with the value of totalJobs and conditionally add an 's' to 'job' if there is more than one job */}
 			<h5>
 				{totalJobs} job{jobs.length > 1 && 's'} found
 			</h5>
+			{/* Render a div element with a className of jobs */}
 			<div className='jobs'>
+				{/* Map over the jobs array and render a Job component for each job */}
 				{jobs.map((job) => {
 					return <Job key={job._id} {...job} />
 				})}
 			</div>
+			{/* Render a div element with a className of center and conditionally render a PageBtnContainer component if numOfPages is greater than 1 */}
 			<div className='center'>{numOfPages > 1 && <PageBtnContainer />}</div>
 		</Wrapper>
 	)
