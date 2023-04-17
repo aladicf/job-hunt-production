c64b619216be22bd81af4704d0706bbbeb0fa957
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 16:54:33 +0200

    Update unauthenticated.js

--- errors/unauthenticated.js
+++ errors/unauthenticated.js
@@ -1,11 +1,15 @@
 import { StatusCodes } from 'http-status-codes'
 import CustomAPIError from './custom-api.js'
 
+// Define an UnAuthenticatedError class that extends the CustomAPIError class
 class UnAuthenticatedError extends CustomAPIError {
-	constructor(message) {
-		super(message)
-		this.statusCode = StatusCodes.UNAUTHORIZED
-	}
+  // Define the class constructor
+  constructor(message) {
+    // Call the super constructor with the message parameter
+    super(message)
+    // Set the statusCode property of the instance to StatusCodes.UNAUTHORIZED (HTTP status code 401)
+    this.statusCode = StatusCodes.UNAUTHORIZED
+  }
 }
 
 export default UnAuthenticatedError
