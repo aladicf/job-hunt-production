f18415c3f931b684d23d30d8534664912cb1cbe0
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 21:08:13 +0200

    Update ProtectedRoute.js

--- client/src/pages/ProtectedRoute.js
+++ client/src/pages/ProtectedRoute.js
@@ -2,24 +2,23 @@ import { useAppContext } from '../context/appContext'
 import { Navigate } from 'react-router-dom'
 import Loading from '../components/Loading'
 
-// Define ProtectedRoute component
+{/* Define ProtectedRoute component */ }
 const ProtectedRoute = ({ children }) => {
-	// Get user and userLoading values from app context
+	{/* Get user and userLoading values from app context */ }
 	const { user, userLoading } = useAppContext()
 
-	// If userLoading is true, render Loading component
+	{/* If userLoading is true, render Loading component */ }
 	if (userLoading)
 		return (
 			<div>
 				<Loading center />
 			</div>
 		)
-
-	// If user is not defined, redirect to /landing route
+	{/* If user is not defined, redirect to /landing route */ }
 	if (!user) {
 		return <Navigate to='/landing' />
 	}
-	// If user is defined, render children
+	{/* If user is defined, render children */ }
 	return children
 }
 
