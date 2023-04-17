ce02a73548d52ed0c8b3eef0531b6c96d117c696
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 20:18:54 +0200

    Update StatsContainer.js

--- client/src/components/StatsContainer.js
+++ client/src/components/StatsContainer.js
@@ -4,10 +4,12 @@ import { MdOutlinePendingActions } from 'react-icons/md'
 import { FaRegCalendarCheck, FaWindowClose } from 'react-icons/fa'
 import Wrapper from '../assets/wrappers/StatsContainer'
 
+// Define a functional component called StatsContainer
 const StatsContainer = () => {
+	// Destructure values from the useAppContext hook
 	const { stats } = useAppContext()
 
-	const defaultStats = [
+	// Define an array of objects called defaultStats
 		{
 			title: 'pending applications',
 			count: stats.pending || 0,
@@ -33,6 +35,7 @@ const StatsContainer = () => {
 
 	return (
 		<Wrapper>
+			// Map over defaultStats array and for each item return a StatItem component with props spread from item object and key set to index
 			{defaultStats.map((item, index) => {
 				return <StatItem key={index} {...item} />
 			})}
