64924f84b7f4d5cbaba0af7b9ef2fd56dc179d91
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 16:29:40 +0200

    Update validateLogin.js

--- utils/validateLogin.js
+++ utils/validateLogin.js
@@ -1,13 +1,18 @@
+
 import { BadRequestError } from '../errors/index.js'
 
+// Defining a function to validate the login fields
 const validateLoginFields = (email, password,) => {
+    // Checking if all values are provided
     if (!email || !password) {
+        // Throwing an error if any value is missing
         throw new BadRequestError('Please provide all values')
     }
 
-    // Validate email format
+    // Validate email format using a regular expression
     const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (!emailRegex.test(email)) {
+        // Throwing an error if the email format is invalid
         throw new BadRequestError('Invalid email format')
     }
 }
