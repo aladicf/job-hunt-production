02470f30a400eddf9e38195de3b7233d7d41fe51
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 16:52:48 +0200

    Update custom-api.js

--- errors/custom-api.js
+++ errors/custom-api.js
@@ -1,7 +1,10 @@
+// Define a CustomAPIError class that extends the built-in Error class
 class CustomAPIError extends Error {
-	constructor(message) {
-		super(message)
-	}
+  // Define the class constructor
+  constructor(message) {
+    // Call the super constructor with the message parameter
+    super(message)
+  }
 }
 
 export default CustomAPIError
