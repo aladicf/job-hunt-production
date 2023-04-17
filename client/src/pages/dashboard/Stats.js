bbabc60f6675874c2c9cbb21259bc0c6c52dc739
Filip Aladić <91838897+aladicf@users.noreply.github.com>
2023-04-17 18:08:17 +0200

    Update Stats.js

--- client/src/pages/dashboard/Stats.js
+++ client/src/pages/dashboard/Stats.js
@@ -2,19 +2,25 @@ import { useEffect } from 'react'
 import { useAppContext } from '../../context/appContext'
 import { StatsContainer, Loading, ChartsContainer } from '../../components'
 
+// Define Stats component
 const Stats = () => {
+	// Get showStats, isLoading, and monthlyApplications values from app context
 	const { showStats, isLoading, monthlyApplications } = useAppContext()
 
+	// Call showStats function when component mounts
 	useEffect(() => {
 		showStats()
 		// eslint-disable-next-line
 	}, [])
+	// If isLoading is true, render Loading component
 	if (isLoading) {
 		return <Loading center />
 	}
 	return (
 		<>
+			{/* Render StatsContainer component */}
 			<StatsContainer />
+			{/* Conditional rendering of ChartsContainer component */}
 			{monthlyApplications.length > 0 && <ChartsContainer />}
 		</>
 	)
